0efcebdeaf8623bd5bb73265ccb1bb21dd7589fa
TusharMalviya <tusharmalviya042@gmail.com>
2026-04-02 00:16:28 +0530

    Added the .gitignore in right path & Cover starting basics of recursion

---
+++ DSA_series/com/recursion/PrintNames.java
@@ -0,0 +1,33 @@
+package com.recursion;
+
+public class PrintNames {
+	
+	public void recursiveCall(int n) {
+		if(n==0 || n<0) {
+			return;
+		}
+		System.out.println("Tushar");
+		n=n-1;
+		recursiveCall(n);
+	}
+	
+	public static void printNamesUsingRecursiveMethod(int count) {
+		PrintNames obj = new PrintNames();
+		obj.recursiveCall(count);
+	}
+	
+	public static void printNTimes(int i , int n) {
+		if(i>n) return;
+		System.out.println("Tushar count: "+i);
+		printNTimes(i+1, n);
+	}
+	
+	
+	public static void printNumbersInReverseOrder( int n) {
+		if(n<=0) return;
+		
+		System.out.print(n+", ");
+		printNumbersInReverseOrder(n-1);
+	}
+
+}
